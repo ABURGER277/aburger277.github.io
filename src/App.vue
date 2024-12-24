69cabddf8aaafddbad95a81318e14c837fcc4dd2
aburger <aburger277@gmail.com>
2024-12-25 04:03:33 +0900

    Profile.vue 추가

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import CareerTimeline from './components/CareerTimeline.vue';
+import Divider from './components/Common/Divider.vue'
+import Profile from './components/Profile.vue'
 </script>
 
 <template>
+  <Profile/>
+  <Divider/>
   <CareerTimeline/>
 </template>
 
