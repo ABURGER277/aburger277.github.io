c6a6ae2e98361ebaf8797d07dd14fa73dabf9075
aburger <aburger277@gmail.com>
2024-12-25 04:16:14 +0900

    ProjectList.vue 추가

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 import CareerTimeline from './components/CareerTimeline.vue';
 import Divider from './components/Common/Divider.vue'
 import Profile from './components/Profile.vue'
+import ProjectList from './components/ProjectList.vue'
 </script>
 
 <template>
   <Profile/>
   <Divider/>
   <CareerTimeline/>
+  <Divider/>
+  <ProjectList/>
 </template>
 
 <style scoped>
