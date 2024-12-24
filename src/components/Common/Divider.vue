c738863159e0a960fdd7f5b3f72c4bd29850a894
aburger <aburger277@gmail.com>
2024-12-25 03:42:44 +0900

    CarrerTimline 추가

---
+++ src/components/Common/Divider.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="divider"></div>
+</template>
+<style scoped>
+.divider {
+  width: 100%;
+  height: 10px;
+  background: black;
+}
+</style>
